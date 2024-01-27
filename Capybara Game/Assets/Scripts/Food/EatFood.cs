16457cd54e4a7d1d43212e580b2f69eb245eb0d0
JoshuaMaher <jmaher300@caledonian.ac.uk>
2024-01-27 18:06:35 +0000

    Add Crunch SFX

--- Capybara Game/Assets/Scripts/Food/EatFood.cs
+++ Capybara Game/Assets/Scripts/Food/EatFood.cs
@@ -9,11 +9,15 @@ public class EatFood : MonoBehaviour
     public GameObject[] filledIndicators;
     private int indicatorToFill = 0;
 
+    [SerializeField] private AudioSource crunch;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // If the other object that the player is colliding with has the tag "Food", add to the meter and destroy the food
         if (collision.gameObject.tag == "Food") 
         {
+            crunch.Play();
+
             if (indicatorToFill <= 4)
             {
                 filledIndicators[indicatorToFill].SetActive(true);
