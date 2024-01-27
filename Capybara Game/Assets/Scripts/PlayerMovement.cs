56cf95547cfa9df4011d86741b5f420233393ec3
fnuerpod <88922673+fnuerpod@users.noreply.github.com>
2024-01-27 15:12:09 +0000

    chore: add internal commentary to player movement script

--- Capybara Game/Assets/Scripts/PlayerMovement.cs
+++ Capybara Game/Assets/Scripts/PlayerMovement.cs
@@ -17,11 +17,14 @@ public class PlayerMovement : MonoBehaviour
     
     void FixedUpdate()
     {
+        // Get player movement from Unity Input class.
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
 
+        // Normalise the updated Vector2 (magnitude of 1).
         move.Normalize();
 
+        // Set RigidBody velocity.
         rb.velocity = (move * moveSpeed);
     }
 }
